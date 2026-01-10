c4904ed5c11d37a03557e5c1e0e3f2b8e29f9c34
HeliMoreli <>
2026-01-10 20:40:41 +0000

    Conditional Statements

---
+++ bedingte_anweisungen/conditional_statements/main.py
@@ -0,0 +1,12 @@
+# Input variables
+product_type = "Dairy"  
+day_of_week = "Wednesday"
+
+number_of_cows = 10
+
+if number_of_cows < 5: 
+    print("not enough cows")
+elif (number_of_cows >=6) and (number_of_cows <=9):
+    print("let's discuss options")
+else:
+    print("this will be fine")
