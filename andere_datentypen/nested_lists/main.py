9e8f877d557455c1d7b2dabbf8a2dcd66ca401e9
HeliMoreli <>
2026-01-11 07:44:23 +0000

    Nested Lists

---
+++ andere_datentypen/nested_lists/main.py
@@ -0,0 +1,25 @@
+# Creating an inventory
+inventory = ["carrots", "bananas", "apples"]
+
+# Adding a new item
+inventory.append("oranges")
+
+# Removing an outdated item
+inventory.remove("bananas")
+
+# Sorting the inventory
+inventory.sort()
+
+# Checking the result
+print("Updated inventory:", inventory)
+
+my_first_list = ["chalk", "shoes", "shirt"]
+print(my_first_list)
+my_first_list.append("tape")
+print(my_first_list)
+my_first_list.extend(["snack", "mobile phone"])
+print(my_first_list)
+my_first_list.sort()
+print(my_first_list)
+print(my_first_list[3])
+print(my_first_list[-1])
