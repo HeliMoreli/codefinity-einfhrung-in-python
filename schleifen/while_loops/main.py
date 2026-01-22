9fa75a99fe2aba2ed1ffecf23af00405854d4493
HeliMoreli <>
2026-01-22 09:13:32 +0000

    While Loops

---
+++ schleifen/while_loops/main.py
@@ -0,0 +1,13 @@
+start_number = 5
+countdown_values = []
+
+# Countdown using while loop
+current = start_number
+while current >= 1:
+    countdown_values.append(current)
+    current-=1
+    
+    
+if current < 1:
+    print("Discount countdown complete!")
+    print(countdown_values)
